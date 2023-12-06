7daf1066f4038d0204a007f045a00ae092eef18d
JB Tellez <jb.tellez@gmail.com>
2023-12-05 22:09:59 -0800

    uses better key

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 'use strict';
 
-const express = require('express');
 require('dotenv').config();
+const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
+const PORT = process.env.PORT;
 
 const app = express();
 
+// middleware
 app.use(cors());
 
-const PORT = process.env.PORT;
-
+// routes
 app.get('/photos', getPhotos);
+app.get('*', notFound);
+app.use('*', errorHandler);
 
+// helper functions
 async function getPhotos(request, response) {
   const searchQuery = request.query.searchQuery;
 
@@ -22,12 +26,21 @@ async function getPhotos(request, response) {
     const photosResponse = await axios.get(url);
     const photoArray = photosResponse.data.results.map(photo => new Photo(photo));
     response.status(200).send(photoArray);
-  } catch (err) {
-    console.error('error from superagent', err);
-    response.status(500).send('server error');
+  } catch (error) {
+    console.error('error fetching photos', error);
+    next(error);
   }
 }
 
+function notFound(request, response) {
+  response.status(404).send('the page you are looking for is not there');
+}
+
+function errorHandler(error, request, response, next) {
+  response.status(500).send('something went wrong ' + error.message);
+}
+
+// classes
 class Photo {
   constructor(obj) {
     this.img_url = obj.urls.regular;
@@ -36,10 +49,5 @@ class Photo {
   }
 }
 
-app.get('*', notFound);
-
-function notFound(request, response) {
-  response.status(404).send('the page you are looking for is not there');
-}
-
+// start server
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
